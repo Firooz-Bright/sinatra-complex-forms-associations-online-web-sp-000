5fd9da624dd79986ae08874d68b987dce666ec52
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2021-02-06 11:53:20 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -29,8 +29,7 @@ class PetsController < ApplicationController
   end
 
   get '/pets/:id/edit' do
-    
-    @pets = Pet.find(params[:id])
+    @pet = Pet.find_by_id(params[:id])
     @owners = Owner.all
     
     erb :'/pets/edit'
