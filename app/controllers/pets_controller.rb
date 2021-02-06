5c69988d50b921d2efd1155afd97a5001b2fe624
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2021-02-06 11:42:21 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -57,9 +57,9 @@ class PetsController < ApplicationController
        if #!params["owner"]["name"].empty?
          @pet.owner.name = params["owner"]["name"]
           @pet.owner = Owner.find_by_id(params["pet"]["owner_id"])
-        @pet.owner = Owner.create(name: params["owner"]["name"])
+      
        else
-       
+         @pet.owner = Owner.create(name: params["owner"]["name"])
         end
       @pet.save
        redirect "pets/#{@pet.id}"
